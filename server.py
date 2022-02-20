43bb2aa318391f161237459ba984b3d39261c1d1
JKOR <kiritor@qq.com>
2022-02-20 20:25:14 +0800

    修复bug

---
+++ server.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Author: JKOR
+"""健康系统自动填报"""
+
+import json
+
+from uilts import HealthCondition
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+
+    # 读取当前目录data.txt, 添加账号
+    f_obj = open('stus_info.json', 'r')
+    stus = json.loads(f_obj.read())
+
+    for stu in stus:
+        student = HealthCondition(stu["xh"], stu["pwd"], stu["email"])
+        student.submit_health_condition()  # 健康系统及体温填报
+        print(student.account, student.result)
+        if student.temperature_flag:
+            for t_data in student.temperature_data:
+                print("体温填报：", t_data["result"])
+
+        student.send_email()  # 发送邮件告知填报结果
---
+++ server_1.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Author: JKOR
+"""健康系统自动填报"""
+
+import json
+
+from uilts import HealthCondition
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+
+    # 读取当前目录data.txt, 添加账号
+    f_obj = open('stus_info_1.json', 'r')
+    stus = json.loads(f_obj.read())
+
+    for stu in stus:
+        student = HealthCondition(stu["xh"], stu["pwd"], stu["email"])
+        student.temperature_flag = False
+        student.submit_health_condition()  # 健康系统及体温填报
+        print(student.account, student.result)
+        for t_data in student.temperature_data:
+            print("体温填报：", t_data["result"])
+
+        student.send_email()  # 发送邮件告知填报结果
